6133a75a04a1e75f0e1ea277b8de4d93482a62cd
Shrimant Jaruhar <s.jaruhar@gmail.com>
2013-03-18 19:18:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indiaelections::Application.config.secret_token = '951996dabec1a6b8f69f85129dccdcfa7c9b1867cdd05f8afb908db3a9bc4b128f91bbf963e3bf1ea6012a36024efee3ab58644736bda97fb856431721e110ca'
