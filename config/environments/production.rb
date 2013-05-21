7b0605364b8e9d5ed4a0b88f17165cd62e5b3bdb
Shrimant Jaruhar <s.jaruhar@gmail.com>
2013-05-21 15:25:54 -0500

    fixing precompile issue attempt 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Indiaelections::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.initialize_on_precompile = false
   
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
@@ -66,4 +66,9 @@ Indiaelections::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+
+
+  config.assets.initialize_on_precompile = false
 end
