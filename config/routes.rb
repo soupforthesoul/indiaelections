1ce36b8205c5c3404aff622a167266979ece0c59
Shrimant Jaruhar <s.jaruhar@gmail.com>
2013-05-23 09:16:27 -0500

    removed public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Indiaelections::Application.routes.draw do 
  
+ root :to => 'constituencies#index'
 
 
 
@@ -68,7 +69,7 @@ Indiaelections::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'constituencies#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
